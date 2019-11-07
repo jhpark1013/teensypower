a2bc0fe1d9c40e3f6328fb7b1ae480d44577264e
Jaehee <jhpark1013@gmail.com>
2019-11-07 15:59:12 -0500

    created package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    author = "Jaehee Park",
+    author_email = "jaehee@ramonaoptics.com",
+    description = "teensy controlled power switch",
+    name = "teensypower",
+    packages=find_packages(include=['teensypower']),
+    version='0.0.0',
+)
