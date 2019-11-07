1b9ba7eeae5e6a3d7f745ed0da3e491f582c579a
Jaehee <jhpark1013@gmail.com>
2019-11-07 17:26:00 -0500

    added versioneer

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import versioneer
 requirements = ['teensytoany',]
 setup(
     author = "Jaehee Park",
@@ -7,7 +8,8 @@ setup(
     install_requires = requirements,
     name = "teensypower",
     packages=find_packages(include=['teensypower']),
-    version='0.0.1',
     python_requires='>=3.6',
     zip_safe=False,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
